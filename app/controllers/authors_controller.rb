edf64093f24a355f8505d75ef6178b005f83e250
Runnan Zhong <r.tina.zhong@gmail.com>
2017-04-12 20:39:58 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,12 +4,19 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @author = Author.new(author_params)
+    @author.save
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.create(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+       render 'new'
+    end
 
-    redirect_to author_path(@author)
+    # redirect_to author_path(@author) re-renders the form if the new author is invalid
   end
 
   private
